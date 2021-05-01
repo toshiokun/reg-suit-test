92b49b3b9b6f651d34c0d3fea221fb280bd12166
toshiokun <toshihiro.taniguchi@cotree.jp>
2021-05-01 20:29:51 +0900

    feat: js用のコンポーネントの作成

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { initScreenshot } from 'storybook-chrome-screenshot';
 
 // Load stories
-const req = require.context('../src', true, /\.stories\.tsx$/);
+const req = require.context('../src', true, /\.stories\.js$/);
 
 configure(() => {
   req.keys().forEach((filename) => req(filename));
 }, module);
+
+// Initialize `storybook-chrome-screenshot`
+addDecorator(initScreenshot());
