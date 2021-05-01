1af1ec8082d95de8ed6aabf60ed6f233ca37206c
toshiokun <toshihiro.taniguchi@cotree.jp>
2021-05-01 21:06:15 +0900

    feat: typescript化

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 // Load stories
-const req = require.context('../src', true, /\.stories\.js$/);
+const req = require.context('../src', true, /\.stories\.tsx$/);
 
 configure(() => {
   req.keys().forEach((filename) => req(filename));
