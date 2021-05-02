129faa233b86fec1621f94608ec9acd0d37baa62
toshiokun <toshihiro.taniguchi@cotree.jp>
2021-05-02 13:43:13 +0900

    Update button styles

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,27 +1,30 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren } from 'react'
 
 interface Props {
-    primary?: boolean;
+  primary?: boolean
 }
 
-export const Button: FC<Props> = ({ children, primary }: PropsWithChildren<Props>) => {
-    return (
-        <button
-    style={{
-      display: 'inline-block',
-      padding: '0 2em',
-      height: 50,
-      borderRadius: 50,
-      borderStyle: 'solid',
-      borderWidth: 1,
-      borderColor: primary ? '#00c4a7' : '#adadad',
-      background: primary ? '#00c4a7' : '#fff',
-      color: primary ? '#fff' : '#363636',
-      font: 'normal 16px/50px sans-serif',
-      textRendering: 'optimizeLegibility',
-    }}
-  >
-    {children}
-  </button>
-    )   
-};
+export const Button: FC<Props> = ({
+  children,
+  primary,
+}: PropsWithChildren<Props>) => {
+  return (
+    <button
+      style={{
+        display: 'inline-block',
+        padding: '0 3em',
+        height: 50,
+        borderRadius: 50,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: primary ? '#00c4a7' : '#adadad',
+        background: primary ? '#00c4a7' : '#fff',
+        color: primary ? '#fff' : '#363636',
+        font: 'normal 14px/50px sans-serif',
+        textRendering: 'optimizeLegibility',
+      }}
+    >
+      {children}
+    </button>
+  )
+}
