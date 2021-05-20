812394f2b8dfa4298f271c0b6f1ac931755dbf91
toshiokun <toshihiro.taniguchi@cotree.jp>
2021-05-20 18:01:11 +0900

    feat: styled-componentsに変更

--- src/Label.tsx
+++ src/Label.tsx
@@ -1,5 +1,5 @@
 import { FC, PropsWithChildren } from 'react'
-
+import styled, { css } from 'styled-components'
 interface Props {
   primary?: boolean
 }
@@ -8,21 +8,25 @@ export const Label: FC<Props> = ({
   children,
   primary,
 }: PropsWithChildren<Props>) => {
-  return (
-    <span
-      style={{
-        display: 'inline-block',
-        padding: '0 3em',
-        height: 55,
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: primary ? 'red' : '#adadad',
-        background: primary ? 'red' : '#fff',
-        color: primary ? '#fff' : '#363636',
-        font: 'normal 14px/55px sans-serif',
-      }}
-    >
-      {children}
-    </span>
-  )
+  return <Body primary={primary}>{children}</Body>
 }
+
+const Body = styled.span`
+  display: inline-block;
+  padding: 0 3em;
+  height: 55;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #adadad;
+  background: #fff;
+  color: #363636;
+  font: normal 14px/55px sans-serif;
+
+  ${(props: any) =>
+    props.primary &&
+    css`
+      border-color: red;
+      background: red;
+      color: #fff;
+    `}
+`
